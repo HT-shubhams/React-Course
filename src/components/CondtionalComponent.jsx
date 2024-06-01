f2bacfecd8c824b069ce4273683beccbc9689176
Shubham Samarth <shubhams@heaptrace.net>
2024-06-01 12:27:01 +0530

    conditional rendering using element varaibles

--- src/components/CondtionalComponent.jsx
+++ src/components/CondtionalComponent.jsx
@@ -1,11 +1,19 @@
-import Code from "./Code";
-import Welcome from "./Welcome";
-
 export default function CondtionalComponent() {
+  /*
+  //////////// This is not suggested as React component should have only one return statement.
+  let messageOne = <h1>This is message One!</h1>;
+  let messageTwo = <h1>This is message Two!</h1>;
+  const display = true;
+
+  if (display) return messageOne;
+  else return messageTwo;
+  */
+
+  let message;
   const display = false;
-  if (display) {
-    return <Welcome />;
-  } else {
-    return <Code />;
-  }
+
+  if (display) message = <h1>This is message One!</h1>;
+  else message = <h1>This is message Two!</h1>;
+
+  return message;
 }
