c8c1758b0849f51cd074c4312b9a33b55ec6dc40
Shubham Samarth <shubhams@heaptrace.net>
2024-06-01 11:24:45 +0530

    map function: array of objects

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,10 +1,19 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Banana", "Orange"];
+  //   const fruits = ["Apple", "Mango", "Banana", "Orange"];
+  const fruits = [
+    { name: "Apple", price: 10, emoji: "🍎" },
+    { name: "Mango", price: 7, emoji: "🥭" },
+    { name: "Orange", price: 2, emoji: "🍊" },
+    { name: "Pineapple", price: 8, emoji: "🍍" },
+  ];
+
   return (
     <>
       <ul>
         {fruits.map((fruit) => (
-          <li key={fruit}>{fruit}</li>
+          <li key={fruit.name}>
+            {fruit.name} {fruit.price}$ {fruit.emoji}
+          </li>
         ))}
       </ul>
     </>
